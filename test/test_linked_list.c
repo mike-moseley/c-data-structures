9251b624f6f60ff160f8e18d43d1bbd7e859506e
Mike Moseley <mike.moseley@tuta.io>
2026-03-13 18:09:41 -0400

    Add more tests for singly linked lists

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -48,6 +48,38 @@ void test_pushNodeNullList(void) {
 	TEST_ASSERT_EQUAL_INT(0, pushNode(list, NULL));
 }
 
+void test_appendNode(void) {
+	linked_list_t *list;
+	char *data1;
+	char *data2;
+
+	list = createLinkedList();
+
+	data1 = "Hello ";
+	data2 = "World!";
+	appendNode(list, data1);
+	appendNode(list, data2);
+
+	TEST_ASSERT_EQUAL_INT(list->size,2);
+	TEST_ASSERT_TRUE(list->head->data == data1);
+	TEST_ASSERT_TRUE(list->head->next->data == data2);
+	free(list->head->next);
+	free(list->head);
+	free(list);
+}
+void test_appendNodeNullData(void) {
+	linked_list_t *list;
+	list = createLinkedList();
+	TEST_ASSERT_EQUAL_INT(1, appendNode(list, NULL));
+	free(list->head);
+	free(list);
+}
+void test_appendNodeNullList(void) {
+	linked_list_t *list;
+	list = NULL;
+	TEST_ASSERT_EQUAL_INT(0, appendNode(list, NULL));
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
@@ -55,5 +87,8 @@ int main(void)
 	RUN_TEST(test_pushNode);
 	RUN_TEST(test_pushNodeNullData);
 	RUN_TEST(test_pushNodeNullList);
+	RUN_TEST(test_appendNode);
+	RUN_TEST(test_appendNodeNullData);
+	RUN_TEST(test_appendNodeNullList);
 	return UNITY_END();
 }
